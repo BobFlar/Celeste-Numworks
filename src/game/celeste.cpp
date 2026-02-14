25b93990e4523d087134e6981903e0fda959cc5a
Nohan <nohanperino1@gmail.com>
2026-02-15 00:39:24 +0100

    Update celeste.cpp

--- src/game/celeste.cpp
+++ src/game/celeste.cpp
@@ -1787,7 +1787,7 @@ void Celeste_P8_update() {
 			start_game_flash-=1;
 			if (start_game_flash<=-30) {
 				begin_game();
-				if (gameState) {
+				/*if (gameState) {
 					is_not_title=true;
 					has_dashed=false;
 			        if (pauseEmu) { pauseEmu = false; }
