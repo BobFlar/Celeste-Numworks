a5f1dc29b1a2d481e017353c635d2d039ec987b4
Nohan <nohanperino1@gmail.com>
2026-02-14 18:19:41 +0100

    Fix game state allocation and loading logic
    Allocate memory for game state if not already allocated and load the state correctly.

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,8 @@ bool running = true;
 uint64_t frameEndTime    = 0;
 uint64_t frameStartTime  = 0;
 
+void* gameState = NULL;
+
 int main(void) {
     // Clearing the screen first
     Display::pushRectUniform(Screen::Rect, 0x000000);
@@ -27,8 +29,8 @@ int main(void) {
 
     Point location = Point(20, 20);
     int read = loadProgressSave();
-    
-    Celeste_P8_load_state(read);
+    gameState = gameState ? gameState : malloc(Celeste_P8_get_state_size());
+    Celeste_P8_load_state(gameState);
     
     while (running) { // You can always exit by double pressing home so \(°-°)/
         if (state.keyDown(Keyboard::Key::Home)) { running = false; }
