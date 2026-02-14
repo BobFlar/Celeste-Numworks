5c1e4cb7a0a26e70fc5400c2120bb4a8a3df2a0b
Nohan <nohanperino1@gmail.com>
2026-02-14 23:51:25 +0100

    Update celeste.cpp

--- src/game/celeste.cpp
+++ src/game/celeste.cpp
@@ -1921,8 +1921,8 @@ void Celeste_P8_draw() {
 	// credits
 	if (is_title()) {
         P8print("OK+BACK",50,80,5);
-        P8print("Nohan",42,88,5);
-        P8print("Fils de pute",46,96,5);
+        P8print("Nohan",51,88,5);
+        P8print("Fils de pute",37,96,5);
     }
 
    
