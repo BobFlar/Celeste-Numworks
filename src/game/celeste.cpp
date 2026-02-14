ec89330aec38617f013ce29f4f542bf2dbcc7a90
Nohan <nohanperino1@gmail.com>
2026-02-14 23:06:02 +0100

    Adjust credit display positions in celeste.cpp

--- src/game/celeste.cpp
+++ src/game/celeste.cpp
@@ -1930,8 +1930,8 @@ void Celeste_P8_draw() {
 	// credits
 	if (is_title()) {
         P8print("OK+BACK",50,80,5);
-        P8print("Nohan",42,96,5);
-        P8print("Fils de pute",46,102,5);
+        P8print("Nohan",42,88,5);
+        P8print("Fils de pute",46,96,5);
     }
 
    
