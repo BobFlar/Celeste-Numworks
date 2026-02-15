3a8bbc2601b74f3e9ab109b26484d9c7138e301a
Nohan <nohanperino1@gmail.com>
2026-02-15 01:05:45 +0100

    Update translation.cpp

--- src/translation.cpp
+++ src/translation.cpp
@@ -506,6 +506,13 @@ void gameMain() {
 	lastEmuBtnState = emuBtnState;
 	emuBtnState = 0;
 	emuInput();
+
+	if (gameState && is_not_title) {
+		if (pauseEmu) { pauseEmu = false; }
+		 Celeste_P8_load_state(gameState);
+	} else {
+		OSDset("Nouvelle Sauvegarde"); 
+	 }
 	
 	if (pauseEmu) {
 		const int textWidth = 8 * 4; 
